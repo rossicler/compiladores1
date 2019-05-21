50554d69386eea011b12f090bd0cbf0a27edf49a
Rossicler <rossiclerjr@gmail.com>
2019-05-21 17:32:48 -0300

    Added reserved keywords and change list of strings to list of objects

--- trab/lexico.py
+++ trab/lexico.py
@@ -5,46 +5,80 @@ class Lexico(object):
     def __init__(self):
         self.p_stack = []
         self.tokens = []
+        self.reserved_words = [
+            'program',
+            'input',
+            'output',
+            ',',
+            '(',
+            ')',
+            ';',
+            'var',
+            'integer',
+            'begin',
+            'end',
+            '.',
+            'read',
+            ':=',
+            '>',
+            '>=',
+            '<',
+            '<=',
+            '<>',
+            ':',
+            'while',
+            'do'
+        ]
         
 
     def main(self):
         # Main function
         texts = input()
         cont = 0
-        for index, character in enumerate(texts):
-            if character == "(":
-                texts = texts[:index+1+cont] + " " + texts[index+1+cont:]
-                cont += 1
-                texts = texts[:index+cont] + " " + texts[index+cont:]
-                cont += 1
-            elif character == ")":
-                texts = texts[:index+cont] + " " + texts[index+cont:]
-                cont += 1
-            elif character == ",":
-                texts = texts[:index+cont] + " " + texts[index+cont:]
-                cont += 1
-            elif character == ";":
-                texts = texts[:index+cont] + " " + texts[index+cont:]
-                cont += 1
+#        for index, character in enumerate(texts):
+#            if character in ["(", ">", "<", ",", ")", ";"]:
+#                texts = texts[:index+cont] + " " + texts[index+cont:]
+#                cont += 1
+#                texts = texts[:index+cont+1] + " " + texts[index+cont+1:]
+#                cont += 1
+        
+        simbols = ["(", ">=", "<=", "<>", ">", "<", ",", ")", ";", ":="]
+
+        for simbol in simbols:
+            if simbol is ">":
+                num = texts.count(simbol)
+                for index in range(0, num):
+                    position = texts.find(simbol)
+                    if not (texts[position-1] == "<" or texts[position+1] == "="):
+                        texts = texts[:position] + " " + texts[position:]
+            elif simbol is "<":
+                num = texts.count(simbol)
+                for index in range(0, num):
+                    position = texts.find(simbol)
+                    if not (texts[position+1] == ">" or texts[position+1] == "="):
+                        texts = texts[:position] + " " + texts[position:]
+            else:
+                texts = texts.replace(simbol, ' ' + simbol + ' ')
 
         texts = texts.split(" ")
-        texts.remove('')
+        texts = list(filter(None, texts))
 
+        import pdb; pdb.set_trace()
+        
         for text in texts:
-            print(text)
-        # for index, text in enumerate(texts):
-        #     for character in text:
-        #         if character == "(":
-                   # Put A in the p_stack for an open parenthesis
-        #             self.p_stack.append("A")
+            if text in self.reserved_words:
+                self.tokens.append({
+                    'cod': 'reserved_word',
+                    'value': text
+                })
+            else:
+                self.tokens.append({
+                    'cod': 'label',
+                    'value': text
+                })
 
-        #         elif character == ")":
-        #             if len(self.p_stack) == 0:
-        #                 print("Rejeito, ')' inesperado")
-        #                 exit()
-        #             else:
-        #                 # Delete the last 'A' in p_stack
-        #                 self.p_stack = self.p_stack[:len(self.p_stack) - 1]
+        for obj in self.tokens:
+            print(obj)
 
 
 prog = Lexico()
