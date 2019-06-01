8c1e8614c8adedb009fa450a7f41cfc1ab47e1bd
Rossicler <rossiclerjr@gmail.com>
2019-05-31 22:36:26 -0300

    Fixed some bugs, but needs to fix the underflow error and the mepa.txt execution

--- trab/exec_mepa.py
+++ trab/exec_mepa.py
@@ -7,19 +7,31 @@ class ExecMEPA():
         self.s = -1
         self.i = 0
         self.my_file_list = []
+        self.labels = {}
         self.n_param = [
             # 0 params
-            ["SOMA", "SUBT", "MULT", "DIVI", "INVR"
-            "CONJ", "DISJ", "NEGA", "CMME", "CMMA",
-            "CMIG", "CMDG", "CMEG", "CMAG", "NADA",
-            "PARA", "LEIT", "IMPR", "INPP"],
+            [
+                "SOMA", "SUBT", "MULT", "DIVI",
+                "INVR", "CONJ", "DISJ", "NEGA",
+                "CMME", "CMMA", "CMIG", "CMDG",
+                "CMEG", "CMAG", "NADA", "PARA",
+                "LEIT", "IMPR", "INPP"
+            ],
             # 1 param
-            ["DSVS", "DSVF", "AMEM", "DMEN", "ENPR"],
+            [
+                "DSVS", "DSVF", "AMEM", "DMEN",
+                "ENPR", "CRCT"
+            ],
             # 2 params
-            ["CRCT"], ["CRVL", "CREN", "ARMZ", "CRVI",
-            "ARMI", "ENRT", "CHPR", "RTPR"],
+            [
+                "CRVL", "CREN", "ARMZ",
+                "CRVI", "ARMI", "ENRT", "CHPR",
+                "RTPR"
+            ],
             # 3 params
-            ["DSVR"]
+            [
+                "DSVR"
+            ]
         ]
 
     
@@ -27,24 +39,77 @@ class ExecMEPA():
         with open(sys.argv[1], 'r') as my_file:
             self.my_file_list = my_file.read().split("\n")
             self.my_file_list = list(filter(None, self.my_file_list))
-        instruction = self.__get_instruction_string(self.my_file_list[2])
+        self.__save_all_labels()
+        self.__init_mem()
+        instruction = ""
+        while instruction != "PARA":
+            line = self.my_file_list[self.i].strip()
+            instruction = self.__get_instruction_string(self.my_file_list[self.i])
+            self.__execute_instruction(instruction, line)
+            self.i += 1
+
+
+    def __init_mem(self):
+        for index in range(0, 1000):
+            self.M.append(0)
+            self.D.append(0)
+
+    
+    def __save_all_labels(self):
+        for index, line in enumerate(self.my_file_list):
+            position = line.find(":")
+            if position != -1:
+                label = line[:position]
+                self.labels[label] = index
+                line = line[position+1:]
         
 
     def __execute_instruction(self, instruction, line):
         if instruction in self.n_param[0]:
-            globals()[instruction]()
-        else if instruction in self.n_param[1]:
-            position = line.find(instruction)
-            param_1 = line[:position]
+            func = getattr(self, instruction)
+            func()
+        elif instruction in self.n_param[1]:
+            position = line.find(" ")
+            param_1 = line[position:]
             param_1 = param_1.strip()
-            globals()[instruction](param_1)
+            func = getattr(self, instruction)
+            if param_1.isdigit():
+                param_1 = int(param_1)
+            func(param_1)
+        elif instruction in self.n_param[2]:
+            position = line.find(" ")
+            params = line[position:]
+            params = params.split(",")
+            params = [param.strip() for param in params]
+            func = getattr(self, instruction)
+            func(int(params[0]), int(params[1]))
+        elif instruction in self.n_param[3]:
+            position = line.find(" ")
+            params = line[position:]
+            params = params.split(",")
+            params = [param.strip() for param in params]
+            func = getattr(self, instruction)
+            func(int(params[0]), int(params[1]), int(params[2]))
 
 
     def __get_instruction_string(self, line):
-        instruction = line.lstrip()
+        instruction = line.strip()
         space_position = instruction.find(" ")
-        instruction = instruction[:space_position]
+        if space_position != -1:
+            instruction = instruction[:space_position]
         return instruction
+
+
+    def __get_label_value(self, label):
+        try:
+            value = self.labels[label]
+            return value
+        except:
+            error_msg = "Linha " + str(self.i + 1) + ": RunTime error rotulo " \
+                + label + " invalido"
+            print(error_msg)
+            exit()
+
 
 
     def CRCT(self, k):
@@ -170,12 +235,12 @@ class ExecMEPA():
 
 
     def DSVS(self, p):
-        self.i = p
+        self.i = self.__get_label_value(p)
 
 
     def DSVF(self, p):
         if self.M[self.s] == 0:
-            self.i = p
+            self.i = self.__get_label_value(p)
         else:
             self.i += 1
         self.s -= 1
