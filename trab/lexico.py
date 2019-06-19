21aab0e6365926b352e3b2fb685623759ea8fcdb
Rossicler <rossiclerjr@gmail.com>
2019-06-19 11:43:56 -0300

    Updated 'sintatico' obj

--- trab/lexico.py
+++ trab/lexico.py
@@ -4,7 +4,6 @@ import sys
 
 class Lexico(object):
     def __init__(self):
-        self.p_stack = []
         self.tokens = []
         self.reserved_words = [
             'program','input','output',',','(',')',
@@ -77,34 +76,87 @@ class Sintatico(object):
     def main(self):
         self.lexico = Lexico()
         self.lexico.build()
+        self.program()
 
     def program(self):
         token = self.lexico.get_token_head()
-        if token.value != "program":
-            self.print_error(token.value, "program")
+        if token['value'] != "program":
+            self.print_error(token['value'], "program")
 
         self.read_label()
 
         token = self.lexico.get_token_head()
-        if token.value != "(":
-            self.print_error(token.value, "(")
+        if token['value'] != "(":
+            self.print_error(token['value'], "(")
 
         self.read_label()
 
         token = self.lexico.get_token_head()
-        while token.value != ')':
-            if token.value != ',':
-                self.print_error(token.value, ",")
+        while token['value'] != ')':
+            if token['value'] != ',':
+                self.print_error(token['value'], ",")
             self.read_label()
             token = self.lexico.get_token_head()
 
         token = self.lexico.get_token_head()
-        if token.value != ';':
-            self.print_error(token.value, ";")
+        if token['value'] != ';':
+            self.print_error(token['value'], ";")
         self.mepa.append("INPP")
+        import pdb; pdb.set_trace()
 
-    def print_error(self, token, expected_token):
-        print("Found an error next to " + token + " it was expected " + expected_token)
+    def block(self):
+        token = self.lexico.get_token_head()
+        if token['value'] == "label":
+            self.label_function()
+        elif token['value'] == "type":
+            self.type_function()
+        elif token['value'] == "var":
+            self.var_function()
+        elif token['value'] == "procedure":
+            self.procedure_function()
+        elif token['value'] == "function":
+            self.function_declaration()
+        elif token['value'] == "begin":
+            self.begin_function()
+        else:
+            self.print_error(token=token['value'])
+
+    def label_function(self):
+        pass
+
+    def type_function(self):
+        pass
+
+    def var_function(self):
+        token = self.lexico.get_token_head()
+        while token['value'] != ':':
+            if token['value'] != ',':
+                self.print_error(token['value'], ",")
+            self.read_label()
+            token = self.lexico.get_token_head()
+        token = self.lexico.get_token_head()
+        if token['value'] != 'integer':
+            self.print_error(token=token['value'])
+        token = self.lexico.get_token_head()
+        if token['value'] != ';':
+            self.print_error(token['value'], ';')
+
+    def procedure_function(self):
+        pass
+
+    def function_declaration(self):
+        pass
+    
+    def begin_function(self):
+        pass
+
+    def type(self):
+
+    def print_error(self, token, expected_token=""):
+        if expected_token:
+            print("Found an error next to " + token + " it was expected " + expected_token)
+        else:
+            print("Found an error next to " + token + "invalid token")
         exit()
 
     def print_error_label(self, token):
@@ -113,14 +165,14 @@ class Sintatico(object):
 
     def check_label(self, label):
         if label[0].isdigit():
-            self.print_error(token.value, "a valid label")
+            self.print_error(token['value'], "a valid label")
         if label in self.vars:
             self.print_error_label(label)
 
     def read_label(self):
         token = self.lexico.get_token_head()
-        self.check_label(token.value)
-        self.vars.append(token.value)
+        self.check_label(token['value'])
+        self.vars.append(token['value'])
 
 
 prog = Sintatico()
